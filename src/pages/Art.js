ee891c8759c4f4a857948496bb394327e517f7d6
Devan Kavalchek <devankav7@gmail.com>
2021-12-03 17:50:16 -0500

    refactored

--- src/pages/Art.js
+++ src/pages/Art.js
@@ -6,16 +6,58 @@ import ImageDisplay from '../components/ImageDisplay';
 import Navbar from '../components/Navbar'
 
 class Art extends Component {
+    constructor (props) {
+        super(props)
+
+        this.images = [
+            {
+                image: "image1.png",
+                label: "Image Title",
+                year: 2021,
+                description: "Description"
+            },
+            {
+                image: "image2.png",
+                label: "Painting",
+                year: 2020,
+                description: "Description"
+            },
+            {
+                image: "image3.png",
+                label: "Another One",
+                year: 2019,
+                description: "Description"
+            },
+            {
+                image: "image4.png",
+                label: "Final",
+                year: 2020,
+                description: "Sculpture"
+            }
+        ]
+    }
+
     render() {
+        let imageDisplays = []
+
+        for (var i = 0; i < this.images.length; i++) {
+            let current = this.images[i]
+
+            imageDisplays.push(<ImageDisplay
+                id={i}
+                image={current.image}
+                label={current.label}
+                year={current.year}
+                description={current.description}
+            />)
+        }
+
         return (
             <Fragment>
                <Navbar/>
 
                 <Col className="Art">
-                    <ImageDisplay image="image1.png" label="Image Title" year={2021} description="Description" />
-                    <ImageDisplay image="image2.png" label="Painting" year={2020} description="Mixed media" />
-                    <ImageDisplay image="image3.png" label="Another One" year={2019} description="Acrylic painting" />
-                    <ImageDisplay image="image4.png" label="Final" year={2020} description="Sculpture" />
+                    {imageDisplays}
                 </Col>
             </Fragment>
         )
