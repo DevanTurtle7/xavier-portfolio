ddf38dedd56d5462f0d7de54cac49552acd012eb
Devan Kavalchek <devankav7@gmail.com>
2021-12-11 20:23:52 -0500

    stubbed delete modal

--- src/admin/components/DeleteButton.js
+++ src/admin/components/DeleteButton.js
@@ -1,17 +1,51 @@
-import { Component } from 'react';
+import { Component, Fragment } from 'react';
 import {
     Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter
 } from 'reactstrap';
 
 class DeleteButton extends Component {
+    constructor(props) {
+        super(props)
 
-    onDelete = () => {
+        this.state = {
+            modalOpen: false
+        }
+    }
+
+    delete = () => {
         this.props.onDelete()
+    }
+
+    openModal = () => {
+        this.setState({ modalOpen: true })
+    }
+
+    closeModal = () => {
+        this.setState({ modalOpen: false })
+    }
+
+    toggle = () => {
+        this.setState({ modalOpen: !this.state.modalOpen })
     }
 
     render() {
         return (
-            <Button color="danger" className="mx-2">Delete</Button>
+            <Fragment>
+                <Button color="danger" className="mx-2" onClick={this.openModal}>Delete</Button>
+                <Modal isOpen={this.state.modalOpen}>
+                    <ModalHeader toggle={this.toggle}>
+                        Are you sure?
+                    </ModalHeader>
+                    <ModalBody>
+                        <Button color="success" className="mx-2" onClick={this.delete}>Yes</Button>
+                        <Button color="danger" className="mx-2" onClick={this.closeModal}>No</Button>
+                    </ModalBody>
+                </Modal>
+            </Fragment>
         )
     }
 }
