c7ffdeefe32beb182cefc710eb42278047aa7590
Devan Kavalchek <devankav7@gmail.com>
2022-02-08 10:05:47 -0500

    updated to aws

--- src/admin/components/UploadButton.js
+++ src/admin/components/UploadButton.js
@@ -1,214 +1,61 @@
-import { Component, Fragment } from 'react';
-import {
-    Button,
-    FormFeedback,
-    FormGroup,
-    Input,
-    Modal,
-    ModalBody,
-    ModalFooter,
-    ModalHeader
-} from 'reactstrap';
-import { MdInsertDriveFile } from 'react-icons/md';
-import { collection, addDoc, updateDoc, doc, getDoc, increment } from "firebase/firestore";
-import { ref, uploadBytesResumable } from "firebase/storage";
+import React, { useState } from 'react';
+import AWS from 'aws-sdk'
+import { getAccesKey, getSecretKey } from '../Credentials';
 
-class UploadButton extends Component {
-    constructor(props) {
-        super(props)
+const S3_BUCKET = 'xavier-portfolio';
+const REGION = 'us-east-2';
 
-        this.state = {
-            files: [],
-            modalOpen: false,
-            description: "",
-            uploading: false,
-            progress: 0
-        }
+AWS.config.update({
+  accessKeyId: getAccesKey(),
+  secretAccessKey: getSecretKey(),
+})
 
-        this.storage = this.props.storage
-        this.db = this.props.db
+const UploadButton = () => {
+  const [file, setFile] = useState(null);
+
+  const myBucket = new AWS.S3({
+    params: { Bucket: S3_BUCKET },
+    region: REGION,
+  })
+
+  const handleFileInput = (e) => {
+    setFile(e.target.files[0]);
+  }
+
+  const uploadFile = (file) => {
+    const params = {
+      ACL: 'public-read',
+      Key: file.name,
+      ContentType: file.type,
+      Body: file,
     }
-
-    imageChanged = (e) => {
-        let files = []
-
-        for (let i = 0; i < e.target.files.length; i++) {
-            let file = e.target.files[i]
-            files.push(file)
-        }
-
-        this.setState({ files: files })
-    }
-
-    validFile = () => {
-        let files = this.state.files
-
-        if (files === []) {
-            return true
-        } else {
-            for (let i = 0; i < files.length; i++) {
-                let file = files[i]
-                let tokens = file.type.split("/")
-                let fileType = tokens[0]
-
-                if (fileType !== "image" && fileType !== "video") {
-                    return false
-                }
-            }
-
-            return true
-        }
-    }
-
-    openModal = () => {
-        this.setState({ modalOpen: true })
-    }
-
-    closeModal = () => {
+    myBucket.putObject(params)
+      .on('httpUploadProgress', (evt) => {
+        // that's how you can keep track of your upload progress
+        /*
         this.setState({
-            file: null,
-            modalOpen: false,
-            description: "",
-            uploading: false,
-            progress: 0
+          progress: Math.round((evt.loaded / evt.total) * 100),
         })
-    }
-
-    toggleModal = () => {
-        if (this.state.modalOpen) {
-            this.closeModal();
-        } else {
-            this.openModal();
+        */
+        console.log('evt time')
+        console.log(evt)
+      })
+      .send((err) => {
+        if (err) {
+          // handle the error here
+          console.log(err)
         }
-    }
+      }).then((e) => {
+        console.log('e time')
+        console.log(e)
+      })
+  }
 
-    upload = async () => {
-        this.setState({ uploading: true })
-        let files = this.state.files
-        let numFiles = files.length
-
-        if (this.validFile() && numFiles > 0) {
-            let description = this.state.description
-            let content = []
-
-            // Generate unique name
-            for (let i = 0; i < numFiles; i++) {
-                let file = files[i]
-                let tokens = file.name.split(".")
-                let numTokens = tokens.length
-                let filetype = tokens[numTokens - 1]
-                let time = new Date().getTime()
-                let name = ""
-
-                for (let i = 0; i < numTokens - 1; i++) {
-                    name += tokens[i]
-                }
-
-                name += "_" + time + "." + filetype
-
-                const storageRef = ref(this.storage, name);
-
-                const uploadTask = uploadBytesResumable(storageRef, file)
-
-                uploadTask.on('state_changed', (snapshot) => {
-                    let progress;
-                    let currentProgress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-
-                    if (numFiles === 1) {
-                        progress = currentProgress
-                    } else {
-                        let percentPer = 100 / (numFiles)
-                        let completed = i * percentPer
-                        progress = completed + (currentProgress / numFiles)
-                    }
-
-                    progress = progress.toFixed(1)
-
-                    this.setState({ progress: progress })
-                })
-
-                await uploadTask.then(async (snapshot) => {
-                    let contentType = snapshot.metadata.contentType
-                    let tokens = contentType.split("/")
-                    let fileType = tokens[0]
-
-                    let current = { filename: name, type: fileType }
-                    content.push(current)
-
-                    if (i === numFiles - 1) {
-                        console.log("creating...")
-                        try {
-                            let collectionRef = collection(this.db, this.props.collection)
-                            let countRef = doc(this.db, "counts", this.props.collection)
-                            let countSnap = await getDoc(countRef)
-                            let size = countSnap.data().count
-
-                            const docRef = await addDoc(collectionRef, {
-                                description: description,
-                                order: size,
-                                content: content,
-                                type: "media"
-                            })
-
-                            await updateDoc(countRef, {
-                                count: increment(1)
-                            })
-
-                            console.log("Document written with ID: ", docRef.id);
-                            this.closeModal()
-                            this.props.onUpload()
-                        } catch (e) {
-                            console.error("Error adding document: ", e);
-                            this.setState({ uploading: false })
-                        }
-                    }
-                });
-            }
-        }
-    }
-
-    descriptionChanged = (e) => {
-        this.setState({ description: e.target.value })
-    }
-
-    render() {
-        let valid = !this.state.uploading
-        let validFileType = this.validFile()
-
-        return (
-            <Fragment>
-                <Button onClick={this.openModal} color="primary" className="fit-content ms-3" disabled>
-                    <MdInsertDriveFile className="me-1 mb-1" />
-                    Upload Media
-                </Button>
-
-                <Modal isOpen={this.state.modalOpen}>
-                    <ModalHeader toggle={this.toggleModal}>
-                        Upload Media
-                    </ModalHeader>
-                    <ModalBody>
-                        <FormGroup>
-                            <Input
-                                type="file"
-                                onChange={this.imageChanged}
-                                className="m-2"
-                                invalid={!validFileType}
-                                multiple
-                            />
-                            <FormFeedback>Invalid file type</FormFeedback>
-                        </FormGroup>
-                        <Input type="text" placeholder="Description" className="m-2" onChange={this.descriptionChanged} />
-                    </ModalBody>
-                    <ModalFooter>
-                        <div className="upload-footer-row">
-                            <Button onClick={this.upload} color="primary" disabled={!valid}>Upload</Button>
-                            <p className='my-auto' hidden={!this.state.uploading}>Uploading {this.state.progress}% done</p>
-                        </div>
-                    </ModalFooter>
-                </Modal>
-            </Fragment>
-        )
-    }
+  return <div>
+    <div>React S3 File Upload</div>
+    <input type="file" onChange={handleFileInput} />
+    <button onClick={() => uploadFile(file)}> Upload to S3</button>
+  </div>
 }
 
 export default UploadButton;
