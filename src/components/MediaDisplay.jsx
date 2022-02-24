2ebe2cb32cebc585cf388327b303d38634c0943a
Devan Kavalchek <devankav7@gmail.com>
2022-02-24 14:11:15 -0500

    refactored media display

--- src/components/MediaDisplay.jsx
+++ src/components/MediaDisplay.jsx
@@ -1,105 +1,124 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import ImageDisplay from './ImageDisplay';
 import VideoDisplay from './VideoDisplay';
 import CarouselControls from './CarouselControls';
 
-class MediaDisplay extends Component {
-    constructor(props) {
-        super(props)
+function MediaDisplay(props) {
+    const [fadeInClass, setFadeInClass] = useState("fade-in-start");
+    const [num, setNum] = useState(0)
 
-        this.state = {
-            fadeInClass: "fade-in-start",
-            num: 0
-        }
+    useEffect(() => {
+        onLoad()
+    }, [])
+
+    const onChange = (num) => {
+        setNum(num)
     }
 
-    onChange = (num) => {
-        this.setState({
-            num: num,
-        })
-    }
-
-    sleep = (milliseconds) => {
+    const sleep = (milliseconds) => {
         return new Promise(resolve => setTimeout(resolve, milliseconds))
     }
 
-    onLoad = async () => {
-        this.setState({ fadeInClass: "fade-in-start" })
-        await this.sleep(1000)
-        this.setState({ fadeInClass: "fade-in-end" })
+    const onLoad = async () => {
+        setFadeInClass("fade-in-start")
+        await sleep(1000)
+        setFadeInClass("fade-in-end")
     }
 
-    render() {
-        let data = this.props.data
+    const getMediaDisplays = () => {
+        let displays = []
+        let data = props.data
         let content = data.content
-        let description = data.description
-        let num = this.state.num
-        let numContent = content.length
-        let media = [];
-        let descriptionClassNames = "image-description"
-        let centered = this.props.centered === true
-        let tag = this.props.tag
-        let viewable = this.props.viewable
-        let link = data.link
+        let tag = props.tag
+        let viewable = props.viewable
 
-        for (let i = 0; i < numContent; i++) {
+        for (let i = 0; i < content.length; i++) {
             let current = content[i]
             let url = current.url
             let type = current.type
 
             if (type === "image") {
-                media.push(<ImageDisplay
+                displays.push(<ImageDisplay
                     url={url}
-                    callback={this.onLoad}
-                    alt={description}
+                    callback={onLoad}
+                    alt={props.description}
                     active={num === i}
                     tag={tag}
                     viewable={viewable}
                     key={i}
                 />)
             } else if (type === "video") {
-                media.push(<VideoDisplay url={url} callback={this.onLoad} key={i} />)
+                displays.push(<VideoDisplay url={url} callback={onLoad} preload="metadata" key={i} />)
             }
         }
 
-        let carouselControls = numContent > 1 ?
-            (<CarouselControls numContent={numContent} onChange={this.onChange}
-            />)
-            : (null)
+        return displays
+    }
 
-        let rowClassNames = this.state.fadeInClass
-        let mediaDisplayClassNames = "media-display"
-
-        if (tag) {
-            mediaDisplayClassNames += " " + tag
-        }
+    const getRowClassNames = () => {
+        let classNames = fadeInClass
+        let centered = props.centered === true
 
         if (centered) {
-            rowClassNames += " centered-row"
-        } else {
-            mediaDisplayClassNames += " media-display-left"
+            classNames += " centered-row"
         }
 
-        let asterisk = (link !== null && link !== undefined && link !== "") ?
-            (<a className='asterisk noselect' href={link} target="_blank">*</a>)
-            : (null)
+        return classNames
+    }
 
+    const getCarouselControls = () => {
+        let data = props.data
+        let content = data.content
+        let numContent = content.length
 
-        return (
-            <div className={rowClassNames}>
-                <div className={mediaDisplayClassNames}>
-                    <div className="media-container">
-                        {asterisk}
-                        {media}
-                    </div>
-                    {carouselControls}
-                    <div className={descriptionClassNames}>
-                        <p>{description}</p>
-                    </div>
+        if (numContent > 1) {
+            return (<CarouselControls numContent={numContent} onChange={onChange} />)
+        } else {
+            return null
+        }
+    }
+
+    const getAsterisk = () => {
+        let data = props.data
+        let link = data.link
+
+        if (link !== null && link !== undefined && link !== "") {
+            return (<a className='asterisk noselect' href={link} target="_blank">*</a>)
+        } else {
+            return null
+        }
+    }
+
+    const getMediaDisplayClassNames = () => {
+        let classNames = "media-display"
+        let centered = props.centered === true
+        let tag = props.tag
+
+        if (tag) {
+            classNames += " " + tag
+        }
+
+        if (!centered) {
+            classNames += " media-display-left"
+        }
+
+        return classNames
+    }
+
+    return (
+        <div className={getRowClassNames()}>
+            <div className={getMediaDisplayClassNames()}>
+                <div className="media-container">
+                    {getAsterisk()}
+                    {getMediaDisplays()}
+                </div>
+                {getCarouselControls()}
+                <div className="image-description">
+                    <p>{props.description}</p>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default MediaDisplay;
