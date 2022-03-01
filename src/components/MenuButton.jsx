f49b8ff246e26f6b3b6c86343adc3a0fc8b0ce03
Devan Kavalchek <devankav7@gmail.com>
2022-03-01 14:48:57 -0500

    refactored and added comments

---
+++ src/components/MenuButton.jsx
@@ -0,0 +1,42 @@
+/**
+ * A button that opens the menu
+ * 
+ * Props:
+ *  NONE
+ * 
+ * @author Devan Kavalchek
+ */
+
+import { useState } from 'react';
+import IconButton from './IconButton';
+import Menu from './Menu';
+import { MdMenu } from "react-icons/md"
+
+function MenuButton(props) {
+    const [open, setOpen] = useState(false)
+
+    /**
+     * Called when the button is clicked. Opens the menu.
+     */
+    const onClick = () => {
+        setOpen(true)
+    }
+
+    /**
+     * Called when the menu is closed. Updates the state to reflect this
+     */
+    const onClose = () => {
+        setOpen(false)
+    }
+
+    return (
+        <div>
+            <IconButton onClick={onClick} className="menu-btn">
+                <MdMenu />
+            </IconButton>
+            <Menu open={open} onClose={onClose} />
+        </div>
+    )
+}
+
+export default MenuButton;
