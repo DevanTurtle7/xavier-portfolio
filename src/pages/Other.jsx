7add2d5dea3c14dd91857a0c85e422ec68c814d6
Devan Kavalchek <devankav7@gmail.com>
2022-03-18 15:21:35 -0400

    fixed media loading

--- src/pages/Other.jsx
+++ src/pages/Other.jsx
@@ -26,6 +26,7 @@ const PAGE_TAG = "other"
 function Other(props) {
     const [stick, setStick] = useState(false)
     const [colorsUpdated, setColorsUpdated] = useState(false)
+    const [media, setMedia] = useState([])
 
     /**
      * Updates the side line stick after each scroll event
@@ -49,7 +50,7 @@ function Other(props) {
             setColorsUpdated(true)
         }
 
-        onScroll()
+        setMedia(props.media)
         window.addEventListener('scroll', onScroll)
 
         return () => {
@@ -57,7 +58,7 @@ function Other(props) {
             // not multiple listeners after the rerender
             window.removeEventListener('scroll', onScroll)
         }
-    }, [stick, colorsUpdated, onScroll])
+    }, [stick, colorsUpdated, onScroll, props.media, media])
 
     /**
      * Creates a media display
@@ -97,7 +98,6 @@ function Other(props) {
      */
     const getDisplays = () => {
         const displays = []
-        const media = props.media;
 
         // Iterate over all the media
         for (let i = 0; i < media.length; i++) {
