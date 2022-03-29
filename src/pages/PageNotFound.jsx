880acbf41fd69bedb2f487f4ddc4e39e3512dd11
Devan Kavalchek <devankav7@gmail.com>
2022-03-29 09:40:09 -0400

    stubbed 404 page

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,37 @@
+import { Fragment, useState, useEffect } from "react"
+import { MetaTags } from "react-meta-tags"
+import BlackoutTitle from "../components/BlackoutTitle"
+
+const ARTIST_NAME = "xavier sylvia-jackson"
+
+const BG_COLOR = "#fff"
+const TEXT_COLOR = "#000"
+const PAGE_TAG = "page-not-found"
+
+function PageNotFound(props) {
+    const [colorsUpdated, setColorsUpdated] = useState(false)
+
+    useEffect(() => {
+        if (!colorsUpdated) {
+            // Set theme colors
+            document.documentElement.style.setProperty('--bs-body-bg', BG_COLOR);
+            document.documentElement.style.setProperty('--bs-body-color', TEXT_COLOR);
+            setColorsUpdated(true)
+        }
+    }, [colorsUpdated])
+
+    return (
+        <Fragment>
+            <MetaTags>
+                <meta name="theme-color" content={BG_COLOR} />
+            </MetaTags>
+            <BlackoutTitle text={ARTIST_NAME} />
+
+            <div className="page">
+                <h1>Hello</h1>
+            </div>
+        </Fragment>
+    )
+}
+
+export default PageNotFound
