d9e1fc4088ac8e49adc9dd2cf9afc4d4e42747c6
Devan Kavalchek <devankav7@gmail.com>
2022-08-14 02:14:44 -0400

    Refactored slice

--- src/shared/components/FolderDisplay.jsx
+++ src/shared/components/FolderDisplay.jsx
@@ -1,48 +1,64 @@
-import { useState } from "react";
-import Modal from "./Modal";
-import TextDisplay from "./TextDisplay";
-import VideoDisplay from "./VideoDisplay";
+import {useState} from 'react';
+import Modal from './Modal';
+import TextDisplay from './TextDisplay';
+import VideoDisplay from './VideoDisplay';
 
 function FolderDisplay(props) {
-  const [modalOpen, setModalOpen] = useState(false)
+  const [modalOpen, setModalOpen] = useState(false);
 
-  const onClick = () => setModalOpen(!modalOpen)
-  const onClose = () => setModalOpen(false)
+  const onClick = () => setModalOpen(!modalOpen);
+  const onClose = () => setModalOpen(false);
 
   return (
     <>
-      <div className="folder-link-container">
-        <button onClick={onClick} className={"folder-link " + props.tag}>{props.data.description}</button>
+      <div className='folder-link-container'>
+        <button onClick={onClick} className={'folder-link ' + props.tag}>
+          {props.data.description}
+        </button>
       </div>
 
       <Modal open={modalOpen} onClose={onClose} scrollable>
-        <div className="folder">
-          <div className="folder-col">
-            {
-              props.data.content.reduce((displaysAccumulator, current, i) => {
-                const type = current.type
-                const key = current.docId + i.toString();
+        <div className='folder'>
+          <div className='folder-col'>
+            {props.data.content.reduce((displaysAccumulator, current, i) => {
+              const type = current.type;
+              const key = current.docId + i.toString();
 
-                if (type === "image") {
-                  return [...displaysAccumulator, (<img src={current.url} className="folder-img" alt="ART" key={key} />)]
-                } else if (type === "video") {
-                  return [...displaysAccumulator, (
-                    <div className="folder-video">
-                      <VideoDisplay url={current.url} key={key} />
-                    </div>
-                  )]
-                } else if (type === "text") {
-                  return [...displaysAccumulator, (<TextDisplay data={current} tag={props.tag + "-folder"} key={key} />)]
-                } else {
-                  return [...displaysAccumulator]
-                }
-              }, [])
-            }
+              if (type === 'image') {
+                return [
+                  ...displaysAccumulator,
+                  <img
+                    src={current.url}
+                    className='folder-img'
+                    alt='ART'
+                    key={key}
+                  />,
+                ];
+              } else if (type === 'video') {
+                return [
+                  ...displaysAccumulator,
+                  <div className='folder-video'>
+                    <VideoDisplay url={current.url} key={key} />
+                  </div>,
+                ];
+              } else if (type === 'text') {
+                return [
+                  ...displaysAccumulator,
+                  <TextDisplay
+                    data={current}
+                    tag={props.tag + '-folder'}
+                    key={key}
+                  />,
+                ];
+              } else {
+                return [...displaysAccumulator];
+              }
+            }, [])}
           </div>
         </div>
       </Modal>
     </>
-  )
+  );
 }
 
 export default FolderDisplay;
--- src/shared/redux/thunks/load_media.jsx
+++ src/shared/redux/thunks/load_media.jsx
@@ -1,129 +1,61 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import { getDocs, collection, query, orderBy } from 'firebase/firestore';
+import {createAsyncThunk} from '@reduxjs/toolkit';
+import {getDocs, collection, query, orderBy} from 'firebase/firestore';
 
 const IMG_URL = 'https://xavier-portfolio.s3.us-east-2.amazonaws.com/';
 
 export const fetchMedia = createAsyncThunk(
   'fetchMedia',
-  async ({ db, collectionName }) => {
-    console.log('Retrieving data...');
-
-    // Get all docs from collection
+  async ({db, collectionName}) => {
     const q = query(collection(db, collectionName), orderBy('order'));
-    const querySnapshot = await getDocs(q)
-    const media = [];
+    const querySnapshot = await getDocs(q);
+    const docs = querySnapshot.docs;
 
-    // Iterate over all of the documents in the collection
-    querySnapshot.docs.forEach((doc) => {
-      const data = doc.data();
-      const type = data.type;
-      const order = data.order;
-      let current;
-
-      if (type === 'media') {
-        const description = data.description;
-        const currentContent = [];
+    return {
+      collection: collectionName,
+      media: docs.reduce((dataAccumulator, doc) => {
+        const data = doc.data();
+        const type = data.type;
         const content = data.content;
 
-        // Iterate over the content
-        for (let i = 0; i < content.length; i++) {
-          const fileInfo = content[i];
-          const fileName = fileInfo.filename;
-          const fileType = fileInfo.type;
+        return [
+          ...dataAccumulator,
+          {
+            ...data,
+            docId: doc.id,
+            content:
+              type === 'text'
+                ? content.replaceAll('$[n]', '\n')
+                : content.reduce((contentAccumulator, current) => {
+                    if (type === 'media') {
+                      return [
+                        ...contentAccumulator,
+                        {
+                          ...current,
+                          url: IMG_URL + current.filename,
+                        },
+                      ];
+                    } else if (type === 'folder') {
+                      const type = current.type;
 
-          const url = IMG_URL + fileName;
-
-          // Save the content to the array
-          currentContent.push({ url: url, type: fileType });
-        }
-
-        // Create a JSON object from this document
-        current = {
-          description: description,
-          order: order,
-          content: currentContent,
-          type: 'media',
-          link: data.link,
-          docId: doc.id,
-        };
-      } else if (type === 'text') {
-        let content = data.content;
-        const size = data.size;
-
-        content = content.replaceAll('$[n]', '\n');
-
-        // Create a JSON object from this document
-        current = {
-          content: content,
-          order: order,
-          type: 'text',
-          size: size,
-          link: data.link,
-          docId: doc.id,
-        };
-      } else if (type === 'folder') {
-        let content = data.content;
-        let description = data.description;
-        let currentContent = [];
-
-        for (let i = 0; i < content.length; i++) {
-          const info = content[i];
-          const currentType = info.type;
-
-          if (currentType === 'image' || currentType === 'video') {
-            const fileName = info.filename;
-            const url = IMG_URL + fileName;
-
-            // Save the content to the array
-            currentContent.push({ url: url, type: currentType });
-          } else if (currentType === 'text') {
-            currentContent.push({
-              content: info.content,
-              type: currentType,
-              size: info.size,
-            });
-          }
-        }
-
-        current = {
-          content: currentContent,
-          order: order,
-          type: 'folder',
-          description: description,
-          docId: doc.id,
-        };
-      } else {
-        console.log('Invalid type: ' + type);
-      }
-
-      if (current !== undefined) {
-        // Insert the current object into the list of media in a sorted manner
-        const mediaCount = media.length;
-
-        if (mediaCount > 0) {
-          // Insert sorted
-          let i = 0;
-          let indexFound = false;
-
-          // Linear search
-          while (i < mediaCount && !indexFound) {
-            let currentOrder = media[i].order;
-
-            if (currentOrder >= order) {
-              indexFound = true;
-            } else {
-              i += 1;
-            }
-          }
-
-          media.splice(i, 0, current);
-        } else {
-          media.push(current);
-        }
-      }
-    });
-
-    console.log('Data retrieved');
-    return { collection: collectionName, media };
+                      if (type === 'image' || type === 'video') {
+                        return [
+                          ...contentAccumulator,
+                          {
+                            ...current,
+                            url: IMG_URL + current.filename,
+                          },
+                        ];
+                      } else if (type === 'text') {
+                        return [...contentAccumulator, current];
+                      } else {
+                        console.log('Invalid type: ' + type);
+                        return [...contentAccumulator];
+                      }
+                    }
+                  }, []),
+          },
+        ];
+      }, []),
+    };
   }
 );
