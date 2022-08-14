0bce19fd488283dba73291141059703b2b821a98
Devan Kavalchek <devankav7@gmail.com>
2022-08-13 20:39:12 -0400

    Refactored

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 /**
  * The main page of the site. Routes to other pages
- * 
+ *
  * Props:
  *  NONE
- * 
+ *
  * @author Devan Kavalchek
  */
 
 import { useCallback, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { collection, getDocs, getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 import Art from './pages/Art';
 import Archive from './pages/Archive';
@@ -24,16 +24,14 @@ import { useDispatch } from 'react-redux';
 import { fetchMedia } from './redux/thunks/load_media';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA1O3ZZUuxv0-PGJPZI9UffooMkAHdyjZw",
-    authDomain: "fir-2e91f.firebaseapp.com",
-    databaseURL: "https://fir-2e91f.firebaseio.com",
-    projectId: "fir-2e91f",
-    storageBucket: "fir-2e91f.appspot.com",
-    messagingSenderId: "352914266642",
-    appId: "1:352914266642:web:7b8ee0cd82b397e3c296cb"
+  apiKey: 'AIzaSyA1O3ZZUuxv0-PGJPZI9UffooMkAHdyjZw',
+  authDomain: 'fir-2e91f.firebaseapp.com',
+  databaseURL: 'https://fir-2e91f.firebaseio.com',
+  projectId: 'fir-2e91f',
+  storageBucket: 'fir-2e91f.appspot.com',
+  messagingSenderId: '352914266642',
+  appId: '1:352914266642:web:7b8ee0cd82b397e3c296cb',
 };
-
-const IMG_URL = "https://xavier-portfolio.s3.us-east-2.amazonaws.com/";
 
 initializeApp(firebaseConfig);
 
@@ -41,26 +39,23 @@ const db = getFirestore();
 const auth = getAuth();
 
 function App(props) {
-    const [artData, setArtData] = useState([])
-    const [archiveData, setArchiveData] = useState([])
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  dispatch(fetchMedia({ db: db, collectionName: 'art' }));
+  dispatch(fetchMedia({ db: db, collectionName: 'other' }));
 
-    dispatch(fetchMedia({db: db, collectionName: 'art'}))
-    dispatch(fetchMedia({db: db, collectionName: 'other'}))
-
-    return (
-        <Router>
-            <Routes>
-                <Route exact path='/' element={<Art media={artData}/>} />
-                <Route exact path='/art' element={<Art media={artData}/>} />
-                <Route exact path='/contact' element={<Contact />} />
-                <Route exact path='/credits' element={<Credits />} />
-                <Route exact path='/archive' element={<Archive media={archiveData} />} />
-                <Route exact path='/admin' element={<Admin db={db} auth={auth} />} />
-                <Route path="*" element={<PageNotFound />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Art />} />
+        <Route exact path="/art" element={<Art />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/credits" element={<Credits />} />
+        <Route exact path="/archive" element={<Archive />} />
+        <Route exact path="/admin" element={<Admin db={db} auth={auth} />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/pages/Archive.jsx
+++ src/pages/Archive.jsx
@@ -1,116 +1,94 @@
 /**
  * The archive page of the site. Displays media on the left hand side of the page,
  * with 2 vertical lines on the far left.
- * 
+ *
  * Props:
  *  media: An array of JSON objects that represent media on the page
- * 
+ *
  * @author Devan Kavalchek
  */
 
 import '../style/archive.css';
 
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment, useEffect } from 'react';
 
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 import MediaDisplay from '../components/MediaDisplay';
 import TextDisplay from '../components/TextDisplay';
 import FolderDisplay from '../components/FolderDisplay';
 import { useSelector } from 'react-redux';
 import { archiveSelector } from '../redux/selectors/archive_selector';
 
-const BG_COLOR = "#000"
-const TEXT_COLOR = "#fff"
-const PAGE_TAG = "archive"
+const BG_COLOR = '#000';
+const TEXT_COLOR = '#fff';
+const PAGE_TAG = 'archive';
 
 function Archive(props) {
-    const [colorsUpdated, setColorsUpdated] = useState(false)
-    const media = useSelector(archiveSelector)
+  const media = useSelector(archiveSelector);
 
-    useEffect(() => {
-        if (!colorsUpdated) {
-            // Set theme colors
-            document.documentElement.style.setProperty('--bs-body-bg', BG_COLOR);
-            document.documentElement.style.setProperty('--bs-body-color', TEXT_COLOR);
-            setColorsUpdated(true)
-        }
-    }, [colorsUpdated])
+  useEffect(() => {
+    // Set theme colors
+    document.documentElement.style.setProperty('--bs-body-bg', BG_COLOR);
+    document.documentElement.style.setProperty('--bs-body-color', TEXT_COLOR);
+  }, []);
 
-    /**
-     * Creates a media display
-     * 
-     * @param {*} data The JSON object of the document
-     * @param {*} key A unique identifier used by React
-     * @returns A MediaDisplay
-     */
-    const createMediaDisplay = (data, key) => {
-        return (<MediaDisplay
-            data={data}
-            tag={PAGE_TAG}
-            viewable={false}
-            key={key}
-        />)
-    }
-
-    /**
-     * Creates a text display
-     * 
-     * @param {*} data The JSON object of the document
-     * @param {*} key A unique identifier used by React
-     * @returns A TextDisplay
-     */
-    const createTextDisplay = (data, key) => {
-        return (<TextDisplay
-            data={data}
-            tag={PAGE_TAG}
-            key={key}
-        />)
-    }
-
-    const createFolderDisplay = (data, key) => {
-        return (<FolderDisplay data={data} tag={PAGE_TAG} key={key} />)
-    }
-
-    /**
-     * Creates all of the displays on the page
-     * 
-     * @return A list of all of the displays
-     */
-    const getDisplays = () => {
-        const displays = []
-
-        // Iterate over all the media
-        for (let i = 0; i < media.length; i++) {
-            const current = media[i]
-            const type = current.type
-            const key = current.docId + i.toString()
-
-            // Create a display
-            if (type === "media") {
-                displays.push(createMediaDisplay(current, key))
-            } else if (type === "text") {
-                displays.push(createTextDisplay(current, key))
-            } else if (type === "folder") {
-                displays.push(createFolderDisplay(current, key))
-            }
-        }
-
-        // Return all displays
-        return displays
-    }
-
+  /**
+   * Creates a media display
+   *
+   * @param {*} data The JSON object of the document
+   * @param {*} key A unique identifier used by React
+   * @returns A MediaDisplay
+   */
+  const createMediaDisplay = (data, key) => {
     return (
-        <Fragment>
-            <Navbar tag={PAGE_TAG} bgColor={BG_COLOR} />
-            <div id={PAGE_TAG}>
-                <div id="displays">
-                    {getDisplays()}
-                </div>
-            </div>
-            <Footer tag={PAGE_TAG} />
-        </Fragment>
-    )
+      <MediaDisplay data={data} tag={PAGE_TAG} viewable={false} key={key} />
+    );
+  };
+
+  /**
+   * Creates a text display
+   *
+   * @param {*} data The JSON object of the document
+   * @param {*} key A unique identifier used by React
+   * @returns A TextDisplay
+   */
+  const createTextDisplay = (data, key) => {
+    return <TextDisplay data={data} tag={PAGE_TAG} key={key} />;
+  };
+
+  const createFolderDisplay = (data, key) => {
+    return <FolderDisplay data={data} tag={PAGE_TAG} key={key} />;
+  };
+
+  /**
+   * Creates all of the displays on the page
+   *
+   * @return A list of all of the displays
+   */
+  const getDisplays = () =>
+    media.reduce((displaysAccumulator, current, i) => {
+      const type = current.type;
+      const key = current.docId + i.toString();
+
+      if (type === 'media') {
+        return [...displaysAccumulator, createMediaDisplay(current, key)];
+      } else if (type === 'text') {
+        return [...displaysAccumulator, createTextDisplay(current, key)];
+      } else if (type === 'folder') {
+        return [...displaysAccumulator, createFolderDisplay(current, key)];
+      }
+    }, []);
+
+  return (
+    <Fragment>
+      <Navbar tag={PAGE_TAG} bgColor={BG_COLOR} />
+      <div id={PAGE_TAG}>
+        <div id="displays">{getDisplays()}</div>
+      </div>
+      <Footer tag={PAGE_TAG} />
+    </Fragment>
+  );
 }
 
 export default Archive;
